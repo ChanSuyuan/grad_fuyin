942a6f17a82bb10026ea53b5e069ff0e776f5c13
Csy <838236432@qq.com>
2021-11-29 15:23:17 +0800

    users chaaracters

--- src/container/ManageCharacters/index.tsx
+++ src/container/ManageCharacters/index.tsx
@@ -1,10 +1,59 @@
-import { Card } from 'antd'
-import React from 'react'
+import { Card, Table } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { adminUsersApi } from './api/adminUsers'
+import { superAdminUsersApi } from './api/superAdminUsers'
+import { IAllUsersFB } from './model/adminUser'
 
 export const ManageCharacters: React.FC = () => {
-  return (
-    <Card>
+  const [store, setStore] = useState<IAllUsersFB>()
+  const [loading, setLoading] = useState<boolean>(false)
+  const type = localStorage.getItem('user_type')
 
+  useEffect(() => {
+    loadPage()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const loadPage = async () => {
+    try {
+      setLoading(true)
+      if (type === "1") {
+        const res = await adminUsersApi.getAllUser()
+        setStore(res)
+      } else if (type === "2") {
+        const res = await superAdminUsersApi.getAllUser()
+        setStore(res)
+      }
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Card title="角色管理">
+      <Table
+        size='middle'
+        loading={loading}
+        dataSource={store.data}
+        pagination={{ pageSize: 10 }}
+        key='ManageCharacters'
+        columns={[
+          {
+            title: "用户名",
+            dataIndex: 'userName',
+            key: 'userName',
+            align: 'center'
+          },
+          {
+            title: "角色",
+            dataIndex: 'type',
+            key: 'type',
+            align: 'center'
+          }
+        ]}
+      />
     </Card>
   )
 }
--- src/container/ManageUsers/index.tsx
+++ src/container/ManageUsers/index.tsx
@@ -1,10 +1,83 @@
-import { Card } from 'antd'
-import React from 'react'
+import { Card, Table } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { superAdminUsersApi } from '../ManageCharacters/api/superAdminUsers'
+import { adminUsersApi } from './api/adminUsers'
+import { IAllUsersFB } from './model/adminUser'
 
 export const ManageUsers: React.FC = () => {
+
+  const [loading, setLoading] = useState<boolean>(false)
+  const [store, setStore] = useState<IAllUsersFB>()
+  const type = localStorage.getItem('user_type')
+
+  useEffect(() => {
+    loadPage()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const loadPage = async () => {
+    try {
+      setLoading(true)
+      if (type === '1') {
+        const res = await adminUsersApi.getAllUser()
+        setStore(res)
+      } else if (type === '2') {
+        const res = await superAdminUsersApi.getAllUser()
+        setStore(res)
+      }
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Card>
-
+      <Table
+        pagination={{ pageSize: 5 }}
+        size='middle'
+        loading={loading}
+        dataSource={store.data}
+        columns={[
+          {
+            title: "用户名",
+            dataIndex: 'userName',
+            key: 'userName',
+            align: 'center'
+          },
+          {
+            title: "爱好",
+            dataIndex: 'hobby',
+            key: 'hobby',
+            align: 'center'
+          },
+          {
+            title: "邮箱",
+            dataIndex: 'email',
+            key: 'email',
+            align: 'center'
+          },
+          {
+            title: "密码",
+            dataIndex: 'password',
+            key: 'password',
+            align: 'center'
+          },
+          {
+            title: "状态",
+            dataIndex: 'state',
+            key: 'state',
+            align: 'center'
+          },
+          {
+            title: "用户角色",
+            dataIndex: 'type',
+            key: 'type',
+            align: 'center'
+          }
+        ]}
+      />
     </Card>
   )
 }
